3f798e16196e94cd537daa05efbc3040ff0ca153
Conrado Moura <cskies@hotmail.com>
2026-06-30 17:33:37 -0300

    fix: add jdbc: prefix in application.properties for DATABASE_URL

--- src/main/java/com/simplifica/SimplificaApplication.java
+++ src/main/java/com/simplifica/SimplificaApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimplificaApplication {
     public static void main(String[] args) {
-        String databaseUrl = System.getenv("DATABASE_URL");
-        if (databaseUrl != null && !databaseUrl.startsWith("jdbc:")) {
-            System.setProperty("spring.datasource.url", "jdbc:" + databaseUrl);
-        }
         SpringApplication.run(SimplificaApplication.class, args);
     }
 }
